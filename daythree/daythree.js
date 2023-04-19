788de134b91a9ec22d94b0867e033bbd8d13b622
vrush-213 <vrushsenbansode@gmail.com>
2023-04-19 13:15:46 +0530

    first commit

---
+++ daythree/daythree.js
@@ -0,0 +1,56 @@
+/*alert("Congratulations")
+alert("Hello");
+alert(4+2);
+
+let num=15;
+alert(num)
+
+var a=15;
+var b= 15;
+var c= a*b;
+alert(c)
+
+const p=501;
+const p2=51;
+const pp=p%p2;
+alert(pp)
+
+var a=15;
+a*= 15;
+alert(a)
+
+const a1=15;
+a1*= 15;
+alert(a1)
+
+
+// relational operator
+
+var xo=12 ==0;
+alert(xo);
+*/
+
+//logical operator
+var a =10
+var b=12 
+alert(a<b && b>a);
+
+if(a<b)
+alert("vrush");
+else
+alert(Bansode);
+
+//type of gives which type if variable it is 
+alert(typeof a)
+
+var vrush="vrushsen";
+
+alert (typeof vrush);
+
+let age =prompt("what i u r age")
+alert(`your age is `+age);
+
+function myFunction() {
+    var x = document.getElementById("myImg").src;
+    document.getElementById("demo").innerHTML = x;
+  }
